b4919b01a026367a9acc6151374aee51b3eea28c
Max Base <MaxBaseCode@Gmail.Com>
2019-08-05 15:19:06 +0430

    Create test.c

---
+++ test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+	printf("%s", "\t\t<header class=\"navbar container\">\n");
+	printf("%s", "\t\t\t<section class=\"navbar-section\" style=\"padding:15px;\">\n");
+	printf("%s", "\t\t\t\t<img src=\"logo.svg\">\n");
+	printf("%s", "\t\t\t\t<form>\n");
+	printf("%s", "\t\t\t\t\t<div class=\"has-icon-left float-right\" style=\"padding-right:20px;\">\n");
+	printf("%s", "\t\t\t\t\t\t<input type=\"text\" class=\"form-input\" style=\"border:0px;border-bottom:2px solid #5755d9;\" placeholder=\"title...\">\n");
+	printf("%s", "\t\t\t\t\t</div>\n");
+	printf("%s", "\t\t\t\t\t<button class=\"btn btn-action btn-sm float-left\" style=\"border:0px;margin-right:-35px;margin-top:6px;\"><i class=\"icon icon-search\" style=\"margin-top:3px;\"></i></button>\n");
+	printf("%s", "\t\t\t\t</form>\n");
+	printf("%s", "\t\t\t</section>\n");
+	printf("%s", "\t\t\t<section class=\"navbar-center\">\n");
+	printf("%s", "\t\t\t\t<ul class=\"tab\">\n");
+	printf("%s", "\t\t\t\t\t<li class=\"tab-item active\">\n");
+	printf("%s", "\t\t\t\t\t\t<a href=\"#\">title</a>\n");
+	printf("%s", "\t\t\t\t\t</li>\n");
+	printf("%s", "\t\t\t\t\t<li class=\"tab-item\">\n");
+	printf("%s", "\t\t\t\t\t\t<a href=\"#\">title</a>\n");
+	printf("%s", "\t\t\t\t\t</li>\n");
+	printf("%s", "\t\t\t\t\t<li class=\"tab-item\">\n");
+	printf("%s", "\t\t\t\t\t\t<a href=\"#\">title</a>\n");
+	printf("%s", "\t\t\t\t\t</li>\n");
+	printf("%s", "\t\t\t\t</ul>\n");
+	printf("%s", "\t\t\t</section>\n");
+	printf("%s", "\t\t\t<section class=\"navbar-section\">\n");
+	printf("%s", "\t\t\t\t<a href=\"#\" class=\"btn btn-link\">title</a>\n");
+	printf("%s", "\t\t\t\t<a href=\"#\" class=\"btn btn-link\">title</a>\n");
+	printf("%s", "\t\t\t</section>\n");
+	printf("%s", "\t\t</header>\n");
+	return 0;
+}
